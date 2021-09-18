2b863655b725aa9414293a5295329b130b98f2d5
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-18 12:06:27 -0400

    install express

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const ExpressError = require("./expressError");
 const Helper = require("./helpers");
 
 const app = express();
+
+app.get("/");
+
+//TODO: figure out if the input is all numbers,
+//  if not, handle error
+
+// need three routes /mean, /median, /mode, all GET requests
