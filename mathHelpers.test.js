025011e1bb5be483e0e78f4fdc877de4bbd569b0
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-18 15:09:47 -0400

    add mode function tests

--- mathHelpers.test.js
+++ mathHelpers.test.js
@@ -12,3 +12,20 @@ describe("mean function", () => {
         expect(num).not.toEqual(expect.any(String));
     });
 });
+
+describe("mode function", () => {
+    test("mode should return an array of most frequent numbers shown in the input array if mode exists", () => {
+        const resultArray = mode([4, 6, 10, 5, 5]);
+        expect(resultArray).toEqual([5]);
+    });
+    test("mode should return an array with a number from the input array if mode exists", () => {
+        const resultArray = mode([4, 6, 10, 5, 5]);
+        expect([4, 6, 10, 5, 5]).toContain(resultArray[0]);
+    });
+    test("mode should return an array", () => {
+        const resultArray = mode([4, 6, 10, 5, 5]);
+        expect(resultArray).toEqual(expect.any(Array));
+        expect(resultArray).not.toEqual(expect.any(String));
+        expect(resultArray).not.toEqual(expect.any(Number));
+    });
+});
